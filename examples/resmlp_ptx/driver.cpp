5b323f3c011796f7f1118505ad0a68df9f85a491
0z5a <dezhen.lu@student.uni-tuebingen.de>
2026-09-24 18:45:49 +0800

    Fuse ResMLP residuals with the following affine in direct PTX
    Signed-off-by: 0z5a <dezhen.lu@student.uni-tuebingen.de>

--- examples/resmlp_ptx/driver.cpp
+++ examples/resmlp_ptx/driver.cpp
@@ -14,3 +14,11 @@ extern "C" int launch_affine(CUfunction function, CUdeviceptr x, CUdeviceptr y,
     void* args[] = {&x, &y, &alpha, &beta, &n, &channels};
     return cuLaunchKernel(function, (n + 1023) / 1024, 1, 1, 256, 1, 1, 0, stream, args, nullptr);
 }
+
+extern "C" int launch_chain(CUfunction function, CUdeviceptr x, CUdeviceptr y,
+                             CUdeviceptr z, CUdeviceptr gamma, CUdeviceptr branch,
+                             CUdeviceptr alpha, CUdeviceptr beta, unsigned n,
+                             CUstream stream) {
+    void* args[] = {&x, &y, &z, &gamma, &branch, &alpha, &beta, &n};
+    return cuLaunchKernel(function, (n + 1023) / 1024, 1, 1, 256, 1, 1, 0, stream, args, nullptr);
+}
